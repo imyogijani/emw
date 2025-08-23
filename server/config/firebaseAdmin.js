3d26115c8b0d912cbda9ae43f6cbe16b985a831f
Meet Chauhan <123140382+Meet1810chauhan@users.noreply.github.com>
2025-08-23 10:05:58 +0530

    fix(config): update Firebase service account key file path

--- server/config/firebaseAdmin.js
+++ server/config/firebaseAdmin.js
@@ -4,7 +4,8 @@ import { readFileSync } from "fs";
 
 // 🔑 Use your Firebase service account key JSON file
 const serviceAccount = JSON.parse(
-  readFileSync(new URL("../firebase/firebaseKey.json", import.meta.url))
+  // readFileSync(new URL("../firebase/firebaseKey.json", import.meta.url))
+  readFileSync(new URL("./serviceAccountKey.json", import.meta.url))
 );
 
 if (!admin.apps.length) {
